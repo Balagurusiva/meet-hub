9f05089e1c2e3c455a3d4bd21cc2e24b1f794910
balaguru <balagurusiva2002@gmail.com>
2024-06-12 14:41:37 +0530

    modified:   app/layout.tsx 	modified:   components/MobileNav.tsx 	modified:   components/Navbar.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "YOOM",
+  title: "MeetHub",
   description: "Video calling App",
   icons: {
     icon: "/icons/logo.svg",
