227508dcb30318ac55b036cd3195d7056ba10f70
balaguru <balagurusiva2002@gmail.com>
2024-06-13 13:12:50 +0530

    modified:   app/(root)/meeting/[id]/page.tsx 	new file:   components/MeetingRoom.tsx 	new file:   components/MeetingSetup.tsx

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,8 +1,22 @@
+
+
 import React from 'react'
 
 const Meeting = ({params} : {params:{id:string}}) => {
+  const {user, isLoaded } = useUser()
+  const [isSetupCompleted, setIsSetupCompletee] = useState(false)
   return (
-    <div>Meeting #{params.id}</div>
+    <main className = 'h-screen w-full'>
+      <StreamCall>
+        <StreamTheme>
+          {!isSetupCompleted ? (
+            "Meeting setup"
+          ):(
+            "Meeting Room"
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
   )
 }
 
